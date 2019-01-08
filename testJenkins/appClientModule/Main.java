c7283a4aca6cb112b391539cc2219a8a39fb3f0c
Stapraq <32795329+stapraqInterface@users.noreply.github.com>
2019-01-08 09:11:55 +0200

    commit 2

--- testJenkins/appClientModule/Main.java
+++ testJenkins/appClientModule/Main.java
@@ -3,6 +3,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Teeeeeeeeeeeeeeeest");
+		System.out.print("Teeeeeeeeeeeeeeeest");
 	}
 
 	/* (non-Java-doc)
